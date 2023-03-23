b188a152062635275280a0171628884ebb9b25db
Naman <128512002+Jaiswal-on-Fire@users.noreply.github.com>
2023-03-23 19:08:33 +0530

    Add files via upload

---
+++ 220687_NAMAN JAISWAL/Assignment_1/First_Q6.py
@@ -0,0 +1,5 @@
+def name_rollno():
+    name = input("Enter your Name:")
+    rollno = int(input("Enter your Roll Number:"))
+    print("Your Name and Roll Number are",name,"and",rollno)
+name_rollno()
---
+++ 220687_NAMAN JAISWAL/Assignment_1/First_Q7.py
@@ -0,0 +1,5 @@
+def Area_Square(a):
+    return a ** 2
+Side = int(input("Enter the Length of Side:"))
+Area = Area_Square(Side)
+print (Area)
