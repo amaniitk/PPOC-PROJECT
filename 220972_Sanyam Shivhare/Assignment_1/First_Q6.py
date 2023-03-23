4595fced9f71806631e9e99e220ea55087e3d86a
SanyamShivhare <121503325+SanyamShivhare@users.noreply.github.com>
2023-03-23 19:55:31 +0530

    Add files via upload

---
+++ 220972_Sanyam Shivhare/Assignment_1/First_Q6.py
@@ -0,0 +1,5 @@
+name = input("Enter your Name:")
+rollno = int(input("Enter your Roll No.:"))
+def name_rollno(a,b):
+    print("Your Name and Roll No. are", a,"and",b)
+name_rollno(name,rollno)
---
+++ 220972_Sanyam Shivhare/Assignment_1/First_Q7.py
@@ -0,0 +1,5 @@
+def Volume(a):
+    return a**3
+Side= int(input("Enter the Length of Side:"))
+Volume = Volume(Side)
+print (Volume)
